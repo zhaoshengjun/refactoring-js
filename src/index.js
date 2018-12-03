9cb3e59c51800e4c6311971b12df720bcdab2d36
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-03 21:48:48 +1000

    write test before starting refactoring

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const statement = require("./statement");
-const plays = require("./plays.json");
-const invoice = require("./invoice.json")[0];
-
-console.log(statement(invoice, plays));
---
+++ src/statement.spec.js
@@ -0,0 +1,16 @@
+const statement = require("./statement");
+const plays = require("./plays.json");
+const invoice = require("./invoice.json")[0];
+
+describe("Statement", () => {
+  it("should works", () => {
+    expect(statement(invoice, plays)).toContain(`Statement for BigCo`);
+    expect(statement(invoice, plays)).toContain(`Hamlet: $650.00 (55 seats)`);
+    expect(statement(invoice, plays)).toContain(
+      `As You Like It: $490.00 (35 seats)`
+    );
+    expect(statement(invoice, plays)).toContain(`Othello: $500.00 (40 seats)`);
+    expect(statement(invoice, plays)).toContain(`Amount owed is $1,640.00`);
+    expect(statement(invoice, plays)).toContain(`You earned 43 credits`);
+  });
+});
