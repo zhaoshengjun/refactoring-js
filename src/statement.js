72efda76185c3299ce8d1a19bbbaf40a4dbc2165
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-03 22:45:49 +1000

    rename var

--- src/statement.js
+++ src/statement.js
@@ -57,19 +57,19 @@ function statement(invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
 
   function totalAmount() {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf, playFor(perf));
+      result += amountFor(perf, playFor(perf));
     }
-    return totalAmount;
+    return result;
   }
 }
 
