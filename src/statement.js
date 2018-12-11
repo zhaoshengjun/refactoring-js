0faf1ecfedbb8ca693d1e754b084887c333d2174
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-11 14:24:57 +1000

    create intermediate data

--- src/statement.js
+++ src/statement.js
@@ -1,8 +1,9 @@
 function statement(invoice, plays) {
-	return renderPlainText(invoice, plays);
+	const statementData = {};
+	return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
 	// main part
 	let result = `Statement for ${invoice.customer}\n`;
 	for (let perf of invoice.performances) {
