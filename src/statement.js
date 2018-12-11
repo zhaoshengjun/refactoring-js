fa7bd878d9c893c7f61e25c19dad4d77f85d13f2
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-11 15:32:40 +1000

    html statement

--- src/statement.js
+++ src/statement.js
@@ -26,4 +26,43 @@ function renderPlainText(data) {
 	}
 }
 
+function htmlStatement(invoice, plays) {
+	return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+	let result = `
+  <h1>Statment for ${data.customer}</h1>
+  <table>
+    <tr>
+      <th>play</th>
+      <th>seats</th>
+      <th>cost</th>
+    </tr>`;
+
+	for (let perf of data.performances) {
+		result += `
+    <tr>
+      <td>${perf.play.name}</td>
+      <td>${perf.audience}</td>
+      <td>${usd(perf.amount)}</td>
+    </tr>
+    `;
+	}
+	result += `
+  </table>
+  <p>Amount owed is <em>${usd(data.totalAmount)}</em></p>  
+  <p>You earned <em>${data.totalVolumeCredits}</em> credits</p>
+  `;
+	return result;
+
+	function usd(amount) {
+		return new Intl.NumberFormat('en-Us', {
+			style: 'currency',
+			currency: 'USD',
+			minimumFractionDigits: 2
+		}).format(amount / 100);
+	}
+}
+
 module.exports = statement;
